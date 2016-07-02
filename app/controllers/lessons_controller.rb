e6ea5cb3258efae007f3cab770a4e50b199bc190
Kevin Hathorn <hathornk@hotmail.com>
2016-07-02 13:37:57 +0000

    Added user authentication for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+      if current_lesson.section.course.user != current_user
+          redirect_to course_path(current_lesson.section.course), alert: 'You must first enroll in the course'
+      end
+  end
+
   helper_method :current_lesson
   def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
