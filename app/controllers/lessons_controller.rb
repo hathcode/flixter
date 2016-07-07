e67e1d901f5b4a8db8f4bdee63c640f1ddd59d59
Kevin Hathorn <hathornk@hotmail.com>
2016-07-07 06:56:10 +0000

    Added 'next lesson' button to lesson page and made it work

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,15 +1,15 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_currently_enrolled, only: [:show]
 
   def show
   end
 
   private
 
-  def require_authorized_for_current_course
-      if current_lesson.section.course.user != current_user
-          redirect_to course_path(current_lesson.section.course), alert: 'You must first enroll in the course'
+  def require_currently_enrolled
+      if ! current_user.enrolled_in?(current_lesson.section.course)
+          redirect_to course_path(current_lesson.section.course), :alert => 'You must first enroll in the course'
       end
   end
 
